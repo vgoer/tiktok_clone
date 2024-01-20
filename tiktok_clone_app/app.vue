35a4261649f70341bc032cd3a4af7a2f11f75865
vgoer <goerblue@gmail.com>
2024-01-20 17:49:50 +0800

    feat: :sparkles: seo和meta

--- tiktok_clone_app/app.vue
+++ tiktok_clone_app/app.vue
@@ -3,4 +3,30 @@
 </template>
 
 <script setup lang="ts">
+
+useHead({
+    title: "my tiktok",
+    meta:[
+        {name: "descrition", content:"我的申请的网站"}
+    ],
+    bodyAttrs:{
+        class:"test"
+    },
+    script:[
+        { innerHTML: 'console.log(\'Hello world\')' } 
+    ]
+})
+
+
+useSeoMeta({
+    title: '我的神奇网站',
+    ogTitle: '我的神奇网站',
+    description: '这是我的神奇网站，让我来告诉你关于它的一切。',
+    ogDescription: '这是我的神奇网站，让我来告诉你关于它的一切。',
+    ogImage: 'https://example.com/image.png',
+    twitterCard: 'summary_large_image',
+})
+
+
+
 </script>

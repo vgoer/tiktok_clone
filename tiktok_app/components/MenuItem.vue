e9b945684f4770c6dbb1e2dece6522089f4bc56b
vgoer <goerblue@gmail.com>
2024-11-13 17:31:24 +0800

    add: 添加侧边栏

---
+++ tiktok_app/components/MenuItem.vue
@@ -0,0 +1,26 @@
+<!-- 菜单 -->
+<template>
+    <div class="w-full flex items-center hover:bg-gray-100 p-2.5 rounded rounded-md">
+        <Icon :name="icon" :color="colorString" :size="sizeString"></Icon>
+        <span
+            :class="`text-[${colorString}]`"
+            class="lg:block hidden pl-[9px] mt-0.5 font-semibold text-[17px]"
+        >
+            {{ iconString }}
+        </span>
+    </div>
+</template>
+<script setup lang='ts'>
+
+const props = defineProps(["iconString","colorString","sizeString"])
+const {iconString, colorString, sizeString} = toRefs(props)
+
+    
+let icon = ref('')
+
+if (iconString?.value === "For You") icon.value = "mdi:home"
+if (iconString?.value === "Following") icon.value = "ci:group"
+if (iconString?.value === "Live") icon.value = "ri:live-line"
+
+
+</script>
--- tiktok_app/components/SideNavMain.vue
+++ tiktok_app/components/SideNavMain.vue
@@ -3,10 +3,14 @@
         id="SideNavMain"
         :class="route.fullPath === '/' ? 'lg:w-[310px]' : 'lg:w[220px]'"
         class=" fixed z-20 bg-white pt-[60px] h-full lg:border-r-0 border-r w-[5px] overflow-auto"
-    >
-    <div>
-        
-    </div>
+    >   
+        <div class="lg:w-full w-[55px] mx-auto">
+            <NuxtLink to="/">
+                <MenuItem iconString="For You" colorString="#F02C56" sizeString="30"></MenuItem>
+            </NuxtLink>
+            <MenuItem iconString="Following" colorString="#000000" sizeString="30"></MenuItem>
+            <MenuItem iconString="Live" colorString="#000000" sizeString="30"></MenuItem>
+        </div>
     </div>
 </template>
 <script setup>
